cb1a92150ef083d0fd9132ab19f4f7fc1b6e85b0
Garrett Jordan <garrettrjordan@gmail.com>
2024-05-02 17:40:20 -0700

    Numbers screen arcs working

--- src/screens/numbers.hpp
+++ src/screens/numbers.hpp
@@ -12,12 +12,7 @@ typedef struct
     lv_obj_t *arc;
     // lv_img_dsc_t img;
     String symbol = LV_SYMBOL_DUMMY;
-    void *updateCB;
 } InfoArc_t;
-
-// void arc1Callback() {
-
-// }
 
 class NumbersScreen
 {
@@ -29,41 +24,57 @@ private:
     InfoArc_t arcs[3];
     uint8_t arcsize = 55;
 
-    void createArc(InfoArc_t arc, int32_t x, int32_t y)
+    void createArc(InfoArc_t *arc, int32_t x, int32_t y)
     {
-        arc.arc = lv_arc_create(scr);
-        lv_obj_align(arc.arc, LV_ALIGN_CENTER, x, y);
-        lv_obj_set_size(arc.arc, arcsize, arcsize);
-        lv_obj_remove_flag(arc.arc, LV_OBJ_FLAG_EVENT_BUBBLE);
+        arc->arc = lv_arc_create(scr);
+        lv_obj_align(arc->arc, LV_ALIGN_CENTER, x, y);
+        lv_obj_set_size(arc->arc, arcsize, arcsize);
+        lv_obj_remove_flag(arc->arc, LV_OBJ_FLAG_EVENT_BUBBLE);
 
-        lv_arc_set_rotation(arc.arc, 180);
+        lv_arc_set_rotation(arc->arc, 180);
 
-        lv_obj_set_style_arc_width(arc.arc, 4, LV_PART_MAIN);
-        lv_obj_set_style_arc_width(arc.arc, 4, LV_PART_INDICATOR);
+        lv_obj_set_style_arc_width(arc->arc, 4, LV_PART_MAIN);
+        lv_obj_set_style_arc_width(arc->arc, 4, LV_PART_INDICATOR);
 
-        lv_obj_remove_flag(arc.arc, LV_OBJ_FLAG_CLICKABLE | LV_OBJ_FLAG_CLICK_FOCUSABLE); /// Flags
-        lv_arc_set_bg_angles(arc.arc, 135, 405);
-        lv_arc_set_range(arc.arc, 0, 100);
-        lv_arc_set_value(arc.arc, 50);
+        lv_obj_remove_flag(arc->arc, LV_OBJ_FLAG_CLICKABLE | LV_OBJ_FLAG_CLICK_FOCUSABLE); /// Flags
+        lv_arc_set_bg_angles(arc->arc, 135, 405);
+        lv_arc_set_range(arc->arc, 0, 100);
+        lv_arc_set_value(arc->arc, 50);
 
-        lv_obj_set_style_bg_opa(arc.arc, 0, LV_PART_KNOB | LV_STATE_DEFAULT);
+        lv_obj_set_style_bg_opa(arc->arc, 0, LV_PART_KNOB | LV_STATE_DEFAULT);
 
-        arc.lbl = lv_label_create(scr);
-        lv_obj_align(arc.lbl, LV_ALIGN_CENTER, x, y);
+        arc->lbl = lv_label_create(scr);
+        lv_obj_align(arc->lbl, LV_ALIGN_CENTER, x, y);
 
-        // arc.icon = lv_img_create(scr);
-        // lv_obj_align(arc.icon, LV_ALIGN_CENTER, x, y - 22);
-        // lv_img_set_src(arc.icon, &arc.img);
-        arc.icon = lv_label_create(scr);
-        lv_obj_align(arc.icon, LV_ALIGN_CENTER, x, y - (arcsize / 3));
-        lv_label_set_text(arc.icon, arc.symbol.c_str());
+        // arc->icon = lv_img_create(scr);
+        // lv_obj_align(arc->icon, LV_ALIGN_CENTER, x, y - 22);
+        // lv_img_set_src(arc->icon, &arc->img);
+        arc->icon = lv_label_create(scr);
+        lv_obj_align(arc->icon, LV_ALIGN_CENTER, x, y - (arcsize / 3));
+        lv_label_set_text(arc->icon, arc->symbol.c_str());
     };
 
-    void setArcValue(InfoArc_t arc, int32_t value, char *fmt = "%i")
+    void scheduleCallback(InfoArc_t *arc)
     {
-        lv_arc_set_value(arc.arc, value);
-        lv_label_set_text_fmt(arc.arc, fmt, value);
-    };
+        // lv_arc_set_value(arc->arc, sysinfo.schedule.percent);
+        // lv_label_set_text_fmt(arc->arc, "%s", sysinfo.schedule.next.c_str());
+        lv_arc_set_value(arc->arc, 0);
+        lv_label_set_text_fmt(arc->lbl, "%i", 0);
+    }
+
+    void stepsCallback(InfoArc_t *arc)
+    {
+        // lv_arc_set_value(arc->arc, sysinfo.health.steps);
+        // lv_label_set_text_fmt(arc->arc, "%i", sysinfo.health.steps);
+        lv_arc_set_value(arc->arc, 0);
+        lv_label_set_text_fmt(arc->lbl, "%i", 0);
+    }
+
+    void batteryCallback(InfoArc_t *arc)
+    {
+        lv_arc_set_value(arc->arc, (int32_t)sysinfo.bat.percent);
+        lv_label_set_text_fmt(arc->lbl, "%i%%", sysinfo.bat.percent);
+    }
 
 public:
     void create()
@@ -94,17 +105,21 @@ public:
         arcs[1].symbol = LV_SYMBOL_IMAGE;
         arcs[2].symbol = LV_SYMBOL_BATTERY_FULL;
 
-        createArc(arcs[0], -58, -64);
-        createArc(arcs[1], -85, 0);
-        createArc(arcs[2], -58, 64);
+        createArc(&arcs[0], -58, -64);
+        createArc(&arcs[1], -85, 0);
+        createArc(&arcs[2], -58, 64);
     };
+
     void periodic()
     {
         lv_label_set_text_fmt(minute, "%02i", sysinfo.time.minute);
         lv_label_set_text_fmt(hour, "%02i", sysinfo.time.hour12);
         lv_label_set_text(date, sysinfo.date.strdate.c_str());
-        // lv_label_set_text_fmt(batnum, "%i%%", sysinfo.bat.percent);
-        // lv_arc_set_value(batarc, sysinfo.bat.percent);
+
+        scheduleCallback(&arcs[0]);
+        stepsCallback(&arcs[1]);
+        batteryCallback(&arcs[2]);
     };
+
     lv_obj_t *scr;
 };
